0275c463d13dcadc99b344ef601d35b635d05032
BlasPiris <bpirisr@gmail.com>
2023-02-13 00:24:35 +0100

    Add components to Home Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { SponsorsComponent } from './sponsors/sponsors.component';
+import { LogoInicialDivComponent } from './logo-inicial-div/logo-inicial-div.component';
+import { InfoDivComponent } from './info-div/info-div.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    SponsorsComponent,
+    LogoInicialDivComponent,
+    InfoDivComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/logo-inicial-div/logo-inicial-div.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-inicial-div',
+  templateUrl: './logo-inicial-div.component.html',
+  styleUrls: ['./logo-inicial-div.component.css']
+})
+export class LogoInicialDivComponent {
+
+}
